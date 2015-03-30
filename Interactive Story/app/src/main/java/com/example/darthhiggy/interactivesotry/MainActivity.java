a56cf9b0cf46f206000ba92e89b7ebba26f19b04
Michael <darthhiggy23@gmail.com>
2015-03-29 22:12:27 -0400

    finished another lesson in Android dev

---
+++ Interactive Story/app/src/main/java/com/example/darthhiggy/interactivesotry/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.darthhiggy.interactivesotry;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    private EditText mNameField;
+    private Button mStartButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNameField = (EditText)findViewById(R.id.naemEditText);
+        mStartButton = (Button)findViewById(R.id.startButton);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameField.getText().toString();
+                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+
+}
